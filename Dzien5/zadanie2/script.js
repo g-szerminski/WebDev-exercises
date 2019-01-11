ec19a779116dcbf6017dfbbdf7eb7dff658769d2
Grzegorz Szermiński <szerman@poczta.fm>
2019-01-11 19:01:13 +0100

    Add jQuery code to day 5-task 2

---
+++ Dzien5/zadanie2/script.js
@@ -0,0 +1,12 @@
+$('.larger').click(function() {
+	$('h1').toggleClass('larger');
+})
+
+$('.smaller').click(function() {
+	$('h1').toggleClass('smaller');
+})
+
+$('.border').click(function() {
+	$('h1').toggleClass('border');
+})
+
