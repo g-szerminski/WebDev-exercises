d9880c8a5312da63f77a3f3a50e98033ae1e63a8
Grzegorz Szermiński <szerman@poczta.fm>
2019-01-30 08:59:04 +0100

    Add task 'Increase and decrease font-size by js ' (day 11-task 1)

---
+++ Dzien11/task1/script.js
@@ -0,0 +1,17 @@
+const btnIncrease = document.querySelector('.bigger');
+const btnDecrease = document.querySelector('.smaller');
+const text = document.querySelector('p');
+
+let textSize = 20;
+text.style.fontSize = textSize + 'px';
+
+function textIncrease() {
+	textSize++;
+	text.style.fontSize = textSize + 'px';
+}
+function textDecrease() {
+	textSize--;
+	text.style.fontSize = textSize + 'px';
+}
+btnIncrease.addEventListener('click', textIncrease);
+btnDecrease.addEventListener('click', textDecrease);
