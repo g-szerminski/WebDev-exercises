75c2f238fb2937f47f1535175c4c5250f1079f31
Grzegorz Szermiński <szerman@poczta.fm>
2019-02-07 19:45:49 +0100

    Change files structure in - Scrolled menu (day 14-project 2)

--- Dzien14/ProjektSkrolowanieMenu/script.js
+++ Dzien14/ProjektSkrolowanieMenu/script.js
@@ -1,18 +1,26 @@
 // $('.me').on('click', function () {
-    //  $('body, html').animate({
-    //   scrollTop: $('.s1').offset().top
-    //  }, 500)
-    // })
+//  $('body, html').animate({
+//   scrollTop: $('.s1').offset().top
+//  }, 500)
+// })
 
-    // $('.portfolio').on('click', function () {
-    //  $('body, html').animate({
-    //   scrollTop: $('.s2').offset().top
-    //  }, 500)
-    // })
+// $('.portfolio').on('click', function () {
+//  $('body, html').animate({
+//   scrollTop: $('.s2').offset().top
+//  }, 500)
+// })
 
-    $('nav a').on('click', function() {
-      const goToSection = "[data-section=" + $(this).attr('class') + "]";
-      $('body, html').animate({
-        scrollTop: $(goToSection).offset().top
-      })
-    })
+
+//    $('nav a').on('click', function() {
+//      const goToSection = "[data-section=" + $(this).attr('class') + "]";
+//      $('body, html').animate({
+//        scrollTop: $(goToSection).offset().top
+//      })
+//    })
+
+$('nav a').on('click', function () {
+  const goToSection = "#" + $(this).attr('class');
+  $('body, html').animate({
+    scrollTop: $(goToSection).offset().top
+  })
+})
